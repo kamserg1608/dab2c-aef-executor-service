cac039de5b329d4eaf7db882cdc7320818f8e5e8
Журавлев Давид Александрович <DAlekZhuravlev@sberbank.ru>
2025-11-10 15:22:27 +0300

    Monitoting connected

--- executor-shared/circuit-breaker/build.gradle.kts
+++ executor-shared/circuit-breaker/build.gradle.kts
@@ -1,6 +1,12 @@
+plugins {
+    id("ru.sbrf.ufs.dab2c.core.shared-conventions")
+}
+
 description = "circuit-breaker"
 
 dependencies {
+    implementation(libs.resilience4j.circuitbreaker)
+    implementation(project(":executor-shared:logging"))
     implementation(project(":executor-shared:commons"))
     testImplementation(project(":executor-shared:test-utils"))
 }
