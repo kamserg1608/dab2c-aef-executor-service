b49a40e6498f282f5c65dc5f57c507f6a1bcb96c
Журавлев Давид Александрович <DAlekZhuravlev@sberbank.ru>
2025-11-10 15:31:58 +0300

    Cbul connected

--- executor-shared/cbul-integration/build.gradle.kts
+++ executor-shared/cbul-integration/build.gradle.kts
@@ -1,9 +1,15 @@
+plugins {
+    id("ru.sbrf.ufs.dab2c.core.shared-conventions")
+}
+
 description = "cbul-integration"
 
 dependencies {
+    api(libs.bundles.ufs.cbul)
     implementation(project(":executor-shared:commons"))
     implementation(project(":executor-shared:monitoring"))
     implementation(project(":executor-shared:circuit-breaker"))
     implementation(project(":executor-shared:annotations"))
+    implementation(project(":executor-shared:logging"))
     testImplementation(project(":executor-shared:test-utils"))
 }
