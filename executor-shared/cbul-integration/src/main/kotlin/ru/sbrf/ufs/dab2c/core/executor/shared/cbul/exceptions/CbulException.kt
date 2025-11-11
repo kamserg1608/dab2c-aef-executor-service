d6433d63f94c0c8d9d4526810370be2ad2e2d03e
Журавлев Давид Александрович <DAlekZhuravlev@sberbank.ru>
2025-11-11 15:17:46 +0300

    Cbul integration deleted

--- executor-shared/cbul-integration/src/main/kotlin/ru/sbrf/ufs/dab2c/core/executor/shared/cbul/exceptions/CbulException.kt
+++
@@ -1,6 +0,0 @@
-package ru.sbrf.ufs.dab2c.core.executor.shared.cbul.exceptions
-
-/**
- * A common error meaning the Cbul response is an error.
- */
-class CbulException(cause: Exception) : Exception(cause.message, cause, false, false)
