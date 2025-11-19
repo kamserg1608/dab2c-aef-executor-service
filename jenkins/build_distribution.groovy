97f20cb3ba77d0583f999f8bc96c9ae429d589e7
Журавлев Давид Александрович <DAlekZhuravlev@sberbank.ru>
2025-11-19 17:01:31 +0300

    Jars compressing

--- jenkins/build_distribution.groovy
+++ jenkins/build_distribution.groovy
@@ -4,18 +4,6 @@ def onDistrib(app, distr) {
     def skipStaticChecks =  env.SKIP_STATIC_CHECKS?.toBoolean()?:false
     gradlew("clean build", skipTests)
 
-    /*
-     * Что здесь происходит:
-     * - мы вычитываем deployment unit-s из свойств Maven-проекта
-     * - при формировании bh-компонента дистрибутива создаём подпапку с именем deploymentUnit, чтобы скопировать джарки именно из этой папки.
-     * Так хитрить приходится именно потому, что из BonJour EFS генерируется проект, и жёстко DU прописывать нельзя.
-     *
-     * Проекты у себя могут внести следующие изменения:
-     * - указать вместо *-application-* свой модуль.
-     * - вместо deploymentUnit* указать конкретную строку, а mavenwEvaluate() удалить.
-     *
-     * Должно соблюдаться правило: если количество deployment == N, то должно быть вызвано 2N distr.addBH()
-     */
     String deploymentUnit1 = "router-executor"
     distr.addConf("./executor-application/configs/build/resources/main/*")
     distr.addBH("./executor-application/deployments/${deploymentUnit1}/build/zero-compressed-main-jar/*.jar", "${deploymentUnit1}-main")
