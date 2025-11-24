2168e725f5aebef58996b79c7bb3f40cb26abe7b
Журавлев Давид Александрович <DAlekZhuravlev@sberbank.ru>
2025-11-24 15:59:44 +0300

    Fix jenkinsfile

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,3 +1,15 @@
+pluginManagement {
+    repositories {
+        maven {
+            credentials {
+                username = System.getProperty("gradle.wrapperUser")
+                password = System.getProperty("gradle.wrapperPassword")
+            }
+            url = uri("https://nexus-ci.delta.sbrf.ru/repository/public/")
+        }
+    }
+}
+
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
