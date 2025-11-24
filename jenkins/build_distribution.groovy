b2f1afbeecb1f20d89dca38aeb765f5792da0467
Журавлев Давид Александрович <DAlekZhuravlev@sberbank.ru>
2025-11-24 16:31:23 +0300

    Fix jenkinsfile

--- jenkins/build_distribution.groovy
+++ jenkins/build_distribution.groovy
@@ -2,7 +2,8 @@ def onDistrib(app, distr) {
 
     def skipTests = env.SKIP_TEST?.toBoolean() ? " -x test " : ""
     def skipStaticChecks =  env.SKIP_STATIC_CHECKS?.toBoolean()?:false
-    gradlew("clean build", skipTests)
+    gradlew("clean :executor-application:deployments:router-executor:dependencies", skipTests)
+    gradlew("clean build", skipTests + " --info --stacktrace ")
 
     String deploymentUnit1 = "router-executor"
     distr.addConf("./executor-application/configs/build/resources/main/*")
