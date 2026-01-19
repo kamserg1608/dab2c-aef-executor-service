ff793c7fe54c8df40198476acf4aaf9d4c2582bc
Журавлев Давид Александрович <DAlekZhuravlev@sberbank.ru>
2026-01-19 11:48:14 +0300

    [CORA-1145][Observer error logging]

--- executor-services/voice-executor/src/main/kotlin/ru/sbrf/dab2c/executor/voice/service/impl/ObservingChunkProcessingServiceDelegate.kt
+++ executor-services/voice-executor/src/main/kotlin/ru/sbrf/dab2c/executor/voice/service/impl/ObservingChunkProcessingServiceDelegate.kt
@@ -2,6 +2,7 @@ package ru.sbrf.dab2c.executor.voice.service.impl
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onEach
 import ru.sbrf.dab2c.executor.domain.voice.VoiceRequest
 import ru.sbrf.dab2c.executor.domain.voice.VoiceResponse
@@ -23,6 +24,7 @@ class ObservingChunkProcessingServiceDelegate(
                 logger.info { "Received request: type=${it::class.simpleName}" }
                 logger.debug { "Received request: type=${it::class.simpleName}, request=$it" }
             }
+            .catch { e -> logger.error(e) { "Error on request stream: ${e.message}" }.also { throw e } }
         return delegate.processRequestChunks(observedChunks)
     }
 
@@ -33,6 +35,7 @@ class ObservingChunkProcessingServiceDelegate(
                 logger.info { "Received response: type=${it::class.simpleName}" }
                 logger.debug { "Received response: type=${it::class.simpleName}, response=$it" }
             }
+            .catch { e -> logger.error(e) { "Error on response stream: ${e.message}" }.also { throw e } }
         return delegate.processResponseChunks(observedChunks)
     }
 }
