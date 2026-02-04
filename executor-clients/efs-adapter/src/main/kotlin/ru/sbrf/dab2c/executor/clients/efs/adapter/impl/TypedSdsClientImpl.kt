d8c9e4283b00b07605fd8788f9614bddb4f102a8
Губеева Наталья Олеговна <NOGubeeva@sberbank.ru>
2026-02-04 19:46:19 +0300

    Правка кук UFS

--- executor-clients/efs-adapter/src/main/kotlin/ru/sbrf/dab2c/executor/clients/efs/adapter/impl/TypedSdsClientImpl.kt
+++ executor-clients/efs-adapter/src/main/kotlin/ru/sbrf/dab2c/executor/clients/efs/adapter/impl/TypedSdsClientImpl.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import ru.sbrf.dab2c.executor.clients.efs.adapter.api.SdsClient
 import ru.sbrf.dab2c.executor.clients.efs.adapter.api.TypedSdsClient
 import ru.sbrf.dab2c.executor.clients.efs.adapter.mapper.DaSessionInfoMapper
-import ru.sbrf.dab2c.executor.clients.efs.adapter.model.SdsSessionInfo
-import ru.sbrf.dab2c.executor.domain.session.DaSessionInfo
+import ru.sbrf.dab2c.executor.clients.efs.adapter.model.SdsSessionMeta
+import ru.sbrf.dab2c.executor.domain.session.DaSessionMeta
 import ru.sbrf.dab2c.executor.domain.session.SdsSection
 
 /**
@@ -70,12 +70,12 @@ private const val SESSION_INFO_KEY_PREFIX = "SESSION_INFO"
  * Read DA session info from SDS.
  * @throws IllegalStateException if session info is not found (invalid session)
  */
-suspend fun TypedSdsClient.readDaSessionInfo(channel: String, cookie: String): DaSessionInfo {
+suspend fun TypedSdsClient.readDaSessionMeta(channel: String, cookie: String): DaSessionMeta {
     val attributeName = "${SESSION_INFO_KEY_PREFIX}_${channel.uppercase()}"
-    val sdsSessionInfo = read<SdsSessionInfo>(
+    val sdsSessionMeta = read<SdsSessionMeta>(
         sectionName = DA_SESSION_SECTION_NAME,
         attributeName = attributeName,
         cookie = cookie
     ) ?: error("Failed to fetch DA session info: session is invalid or not found")
-    return DaSessionInfoMapper.INSTANCE.toDomain(sdsSessionInfo)
+    return DaSessionInfoMapper.INSTANCE.toDomain(sdsSessionMeta)
 }
--- executor-services/voice-executor/src/main/kotlin/ru/sbrf/dab2c/executor/voice/service/impl/SessionInitServiceImpl.kt
+++ executor-services/voice-executor/src/main/kotlin/ru/sbrf/dab2c/executor/voice/service/impl/SessionInitServiceImpl.kt
@@ -2,7 +2,10 @@ package ru.sbrf.dab2c.executor.voice.service.impl
 
 import org.springframework.stereotype.Service
 import ru.sbrf.dab2c.executor.clients.efs.adapter.api.TypedSdsClient
-import ru.sbrf.dab2c.executor.clients.efs.adapter.impl.readDaSessionInfo
+import ru.sbrf.dab2c.executor.clients.efs.adapter.impl.readDaSessionMeta
+import ru.sbrf.dab2c.executor.domain.session.DaSessionCommon
+import ru.sbrf.dab2c.executor.domain.session.DaSessionInfo
+import ru.sbrf.dab2c.executor.domain.session.DaSessionUserInfo
 import ru.sbrf.dab2c.executor.voice.logging.VoiceMdcInitializer
 import ru.sbrf.dab2c.executor.voice.model.RequestHeader
 import ru.sbrf.dab2c.executor.voice.model.ufsCookie
@@ -18,12 +21,36 @@ class SessionInitServiceImpl(
     private val typedSdsClient: TypedSdsClient
 ) : SessionInitService {
 
+    @Suppress("LongMethod")
     override suspend fun initialize() {
         val metadata = currentRequestMetadata()
-        metadata._daSessionInfo = typedSdsClient.readDaSessionInfo(
+        val daSessionMeta = typedSdsClient.readDaSessionMeta(
             metadata.getHeader(RequestHeader.CHANNEL),
             metadata.ufsCookie
         )
+
+        val daSessionCommon = DaSessionCommon(
+            block = "",
+            channel = "",
+            surface = "",
+            platform = "",
+            sdkVersion = "",
+            entryPoint = "",
+            appVersion = "",
+            channelVersion = "",
+            appSource = "",
+            timeZone = ""
+        )
+
+        val daSessionUserInfo = DaSessionUserInfo(
+            firstName = "",
+            patrName = "",
+            birthDay = "",
+            segmentCodeType = "",
+            ucpId = ""
+        )
+
+        metadata._daSessionInfo = DaSessionInfo(daSessionMeta, daSessionCommon, daSessionUserInfo)
 
         val daSessionInfo = metadata.daSessionInfo
         VoiceMdcInitializer.updateWithSessionInfo(
