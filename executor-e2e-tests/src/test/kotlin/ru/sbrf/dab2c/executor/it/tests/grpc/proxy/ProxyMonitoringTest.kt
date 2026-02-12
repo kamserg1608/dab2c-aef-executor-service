0b8e8131f9e061afe35ff4a5e21fc83fa97c41f0
Губеева Наталья Олеговна <NOGubeeva@sberbank.ru>
2026-02-12 13:37:20 +0300

    CORA-1241: фикс тестов

--- executor-e2e-tests/src/test/kotlin/ru/sbrf/dab2c/executor/it/tests/grpc/proxy/ProxyMonitoringTest.kt
+++ executor-e2e-tests/src/test/kotlin/ru/sbrf/dab2c/executor/it/tests/grpc/proxy/ProxyMonitoringTest.kt
@@ -1,8 +1,19 @@
 package ru.sbrf.dab2c.executor.it.tests.grpc.proxy
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.jackson.jackson
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import ru.sbrf.dab2c.executor.clients.gigavoice.proto.GigaVoiceResponse
@@ -16,8 +27,7 @@ import ru.sbrf.dab2c.executor.voice.model.ExecutorVoiceMetric
 class ProxyMonitoringTest : BaseGigaVoiceIntegrationTest() {
 
     @Test
-    fun `should be metrics for incoming and outcoming chunks`() = runItTest {
-
+    fun `should contain metrics for incoming and outgoing chunks in metrics endpoint`() = runItTest {
         GigaVoiceResponse.newBuilder()
         withSession(proxyStub(), mockGigaVoiceService, gigaVoiceAgentMock) {
             session.sendRequest(settingsRequest())
@@ -27,30 +37,24 @@ class ProxyMonitoringTest : BaseGigaVoiceIntegrationTest() {
             session.awaitResponse()
         }
 
-        val response = httpClient.get("$basePath/actuator/metrics")
-
-        print(response.bodyAsText())
-
-        assertThat(
-            response.bodyAsText().contains(ExecutorVoiceMetric.GRPC_INCOMING_FROM_INITIATOR_CHUNKS_TOTAL.metricName)
-        ).isTrue()
-        assertThat(
-            response.bodyAsText().contains(ExecutorVoiceMetric.GRPC_OUTGOING_FROM_INITIATOR_CHUNKS_TOTAL.metricName)
-        ).isTrue()
-    }
-
-    @Test
-    fun `should contain metrics for incoming and outgoing chunks in prometheus endpoint`() = runItTest {
-        GigaVoiceResponse.newBuilder()
-        withSession(proxyStub(), mockGigaVoiceService, gigaVoiceAgentMock) {
-            session.sendRequest(settingsRequest())
-            mock.awaitRequest { it.hasSettings() }
-            val audioResponse = audioResponse(1)
-            mock.sendResponse(audioResponse)
-            session.awaitResponse()
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                jackson {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                }
+            }
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.HEADERS
+            }
+            defaultRequest {
+                url.protocol = URLProtocol.HTTP
+                url.host = "localhost"
+                url.port = 8081
+            }
         }
 
-        val response = httpClient.get("$basePath/actuator/prometheus") {
+        val response = client.get("/actuator/metrics") {
             header("Accept", "text/plain; version=0.0.4; charset=utf-8")
         }
 
--- executor-e2e-tests/src/test/kotlin/ru/sbrf/dab2c/executor/it/tests/http/ManagementEndpointsTest.kt
+++ executor-e2e-tests/src/test/kotlin/ru/sbrf/dab2c/executor/it/tests/http/ManagementEndpointsTest.kt
@@ -1,7 +1,18 @@
 package ru.sbrf.dab2c.executor.it.tests.http
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.URLProtocol
+import io.ktor.serialization.jackson.jackson
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -14,7 +25,23 @@ class ManagementEndpointsTest : BaseGigaVoiceIntegrationTest() {
 
     @Test
     fun actuatorHealth() = runTest {
-        val response = httpClient.get("$basePath/actuator/health")
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                jackson {
+                    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                }
+            }
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.HEADERS
+            }
+            defaultRequest {
+                url.protocol = URLProtocol.HTTP
+                url.host = "localhost"
+                url.port = 8081
+            }
+        }
+        val response = client.get("/actuator/health")
         assertEquals(HttpStatusCode.OK, response.status)
     }
 }
